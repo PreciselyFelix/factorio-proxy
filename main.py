1a2192679eac2abe3b91ac0b4348c1d29880f9d6
Felix <mcfeli@gmx.de>
2024-02-14 12:04:53 +0100

    added cli

--- main.py
+++ main.py
@@ -6,9 +6,79 @@ from network_message.types import MessageType
 
 from proxy import Proxy
 
+import argparse
+
 
 if __name__ == "__main__":
-    action_to_inject = InputAction(InputActionType.StopWalking, 0, EmptyPayload())
-    message_handler = ContinuousInputActionInjectionHandler(action_to_inject, MessageType.ClientToServerHeartbeat)
+    parser = argparse.ArgumentParser(
+        description="Local Proxy to intercept and modify factorio network traffic.",
+        formatter_class=argparse.RawTextHelpFormatter
+    )
+
+    parser.add_argument(
+        "mode",
+        choices=["pass-through", "decode", "inject"],
+        help=(
+            "pass-through: Just passes the messages to the server without decoding or modifying them.\n"
+            "decode: Tries to decode and log each message before re-encoding and sending it to the server.\n"
+            "inject: Same as decode but also tries to inject a specified input action into each message."
+        )
+    )
+
+    parser.add_argument(
+        "--factorio_ip",
+        action="store",
+        default="127.0.0.1",
+        help="IP address of the factorio server to forward to."
+    )
+
+    parser.add_argument(
+        "--factorio_port",
+        action="store",
+        default=34197,
+        help="Port of the factorio server to forward to."
+    )
+
+    parser.add_argument(
+        "--listen_ip",
+        action="store",
+        default="localhost",
+        help="IP address that the proxy should listen on."
+    )
+
+    parser.add_argument(
+        "--listen_port",
+        action="store",
+        default=1234,
+        help="Port that the proxy should listen on."
+    )
+
+    parser.add_argument(
+        "--input_action_type",
+        action="store",
+        help="Only required in injection mode. Type of the input action to inject. Needs to be an exact match for a type defined in input_actions/types.py that accepts an empty payload."
+    )
+
+    parser.add_argument(
+        "--message_type",
+        action="store",
+        help="Only required in injection mode. Must be ClientToServerHeartbeat or ServerToClientHeartbeat."
+    )
+
+    args = parser.parse_args()
+
+    match args.mode:
+        case "pass-through":
+            message_handler = PassThroughHandler()
+        case "decode":
+            message_handler = LoggingHandler()
+        case "inject":
+            if args.input_action_type is None:
+                parser.error(f"--input_action_type is required in injection mode. Needs to be an exact match for a type defined in input_actions/types.py that accepts an empty payload.")
+            if args.message_type is None:
+                parser.error("--message_type is required in injection mode. Must be ClientToServerHeartbeat or ServerToClientHeartbeat.")
+            action_to_inject = InputAction(InputActionType[args.input_action_type], 0, EmptyPayload())
+            message_handler = ContinuousInputActionInjectionHandler(action_to_inject, MessageType[args.message_type])
+
     proxy = Proxy(message_handler)
     proxy.listen()
